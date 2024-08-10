dc5662eb3b93aeb563aa2e84d152572880afae32
FileExpIorer <emit.kax@gmail.com>
2024-08-10 16:41:53 -0500

    Update commands.lua

--- Api/commands.lua
+++ Api/commands.lua
@@ -101,7 +101,7 @@ insertCommand("to", function(getPlayer)
         end; rs:Disconnect()
     end)
 
-    for i = 1, 25 do
+    for i = 1, 5 do
         if (plr.Character) and ((tonumber(plr.Character:FindFirstChild("Humanoid").Health)) > (0)) and (plr.Character:FindFirstChild("HumanoidRootPart")) and ((getPlayer).Character) and ((tonumber((getPlayer).Character:FindFirstChild("Humanoid").Health)) > (0)) and ((getPlayer).Character:FindFirstChild("HumanoidRootPart")) then
             spawn(function()
                 if (plr.Character:FindFirstChild("Humanoid").Sit) and ((tonumber((plr.Character:FindFirstChild("Humanoid").MoveDirection).Magnitude)) <= (0)) then
